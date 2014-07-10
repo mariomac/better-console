40c6741c50d8a71feb58571b7736ade458d8a324
mariomac <mariomac@gmail.com>
2014-07-10 16:33:49 +0200

    ...

--- color.h
+++ color.h
@@ -1,9 +1,6 @@
 #include<sys/ioctl.h>
 #include<stdio.h>
 
-#define FG_INDEX 30
-#define BG_INDEX 40
-
 #define NEGRO 0
 #define ROJO 1
 #define VERDE 2
@@ -12,6 +9,9 @@
 #define MAGENTA 5
 #define CIAN 6
 #define BLANCO 7
+
+#define FG_INDEX 30
+#define BG_INDEX 40
 
 // http://man7.org/linux/man-pages/man4/console_codes.4.html
 void borrar_pantalla() {
@@ -28,6 +28,10 @@ void color(int col) {
 
 void color_fondo(int col) {
 	printf("\033[%dm",col+BG_INDEX);
+}
+
+void colorrva(int r, int g, int b) {
+    printf("\033[38;2;255;255;1m");
 }
 
 int max_filas() {
---
+++ ej6.c
@@ -0,0 +1,8 @@
+#include "color.h"
+
+int main() {
+	printf("hola");
+	colorrva(0,0,0);
+	printf("hola\n");
+	return 0;
+}
